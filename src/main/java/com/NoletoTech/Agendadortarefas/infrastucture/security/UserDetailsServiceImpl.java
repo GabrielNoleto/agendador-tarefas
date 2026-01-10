8ce96f4b7e5613722aa105b59dff5eb2966da04e
Gabriel_Noleto <gabrielsiqueirarn@gmail.com>
2026-01-10 16:57:38 -0300

    Adicionadas configurações de segurança

---
+++ src/main/java/com/NoletoTech/Agendadortarefas/infrastucture/security/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.NoletoTech.Agendadortarefas.infrastucture.security;
+
+
+import ch.qos.logback.core.net.server.Client;
+import com.NoletoTech.Agendadortarefas.business.dto.UsuarioDTO;
+import com.NoletoTech.Agendadortarefas.infrastucture.client.usuarioClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl  {
+
+    @Autowired
+    private usuarioClient client;
+
+    public UserDetails carregaDadosUsuario(String email, String token){
+        UsuarioDTO usuarioDTO = client.buscaPorEmail(email, token);
+        return User
+                .withUsername(usuarioDTO.getEmail()) // Define o nome de usuário como o e-mail
+                .password(usuarioDTO.getSenha()) // Define a senha do usuário
+                .build(); // Constrói o objeto UserDetails
+    }
+
+}
